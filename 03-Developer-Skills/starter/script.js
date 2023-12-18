470ae89754b0a1cea70e662b9428d694195b26b7
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2023-12-18 18:31:09 +0100

    Coding challenge complete

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -7,3 +7,27 @@ if (x === 23) console.log(23);
 const calcAge = birthYear => 2037 - birthYear;
 
 console.log(calcAge(1944));
+
+// Coding Challenge #1
+// Given an array of forecasted maximum temperatures, the thermometer displays a
+// string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+// days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+// Your tasks:
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+// string like the above to the console. Try it with both test datasets.
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  let string = '';
+  for (i = 0; i < arr.length; i++) {
+    string += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log('... ' + string);
+}
+
+printForecast(data2);
+
+// 2. Use the problem-solving framework: Understand the problem and break it up
+// into sub-problems!
